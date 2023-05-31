5fb7832b910a6bc2985b2dda25f88b56177d1214
Yuval Leberstein <94398909+yuvalleberstein21@users.noreply.github.com>
2023-05-31 17:03:58 +0300

    Add files via upload

---
+++ client/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim shirt',
+            slug: 'nike-slim-shirt',
+            category: 'shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+        {
+            _id: '2',
+            name: 'Nike Slim pant',
+            slug: 'nike-slim-pant',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 25,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'high quality product'
+        },
+        {
+            _id: '3',
+            name: 'Adidas Fit Shirt',
+            slug: 'adidas-fit-shirt',
+            category: 'shirts',
+            image: '/images/p2.jpg',
+            price: 250,
+            countInStock: 8,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+        {
+            _id: '4',
+            name: 'Adidas Fit Pant',
+            slug: 'adidas-fit-pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 65,
+            countInStock: 5,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+    ],
+}
+
+export default data;
---
+++ server/db.js
@@ -0,0 +1,20 @@
+// const mongoose = require("mongoose");
+
+// var mongoURL = 'mongodb+srv://yuvalleberstein21:press97vbh@cluster0.ksumt6v.mongodb.net/shopping-cart'
+
+// mongoose.connect(mongoURL, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true
+// })
+
+// var db = mongoose.connection
+
+// db.on('connected', () => {
+//     console.log(`Mongo DB Connection Successfull 🔥`)
+// });
+
+// db.on('error', () => {
+//     console.log(`Mongo DB Connection Failed !`)
+// })
+
+// module.exports = mongoose
